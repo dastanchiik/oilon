348933672de86c6abadb8a5b8b5f30d4b1591428
dastanchick <dosyafree32@gmail.com>
2022-11-13 19:34:52 +0600

    I am added my repositories

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
         int number = scanner.nextInt();
+        System.out.println("Введите число");
         int numbers = scanner.nextInt();
+        System.out.println("Выберите функцию: /     *     -     +");
         String plusOrMinus = scanner.next();
         switch (plusOrMinus) {
             case "/":
@@ -24,17 +27,21 @@ public class Main {
 
     public static void multiply(int a, int b) {
         int equal = a * b;
+        System.out.println(equal);
     }
 
     public static void division(int a, int b) {
         int equals = a / b;
+        System.out.println(equals);
     }
 
     public static void plus(int a, int b) {
         int equalss = a + b;
+        System.out.println(equalss);
     }
 
     public static void minus(int a, int b) {
         int equala = a - b;
+        System.out.println(equala);
     }
 }
